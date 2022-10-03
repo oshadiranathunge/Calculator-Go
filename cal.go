84fd420f9e3c8813bdc4e31dbf6da0081392bd96
Oshadi Ranathunge <45248856+oshadiranathunge@users.noreply.github.com>
2022-10-03 06:24:25 +0530

    Change variable number 2

--- cal.go
+++ cal.go
@@ -3,31 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var num1, number2 float64
+	var num1, num2 float64
 	var operator string
 
 	fmt.Print("Enter number:")
 	fmt.Scanln(&num1)
 
 	fmt.Print("Enter number:")
-	fmt.Scanln(&number2)
+	fmt.Scanln(&num2)
 
 	fmt.Print("Enter operator (+ - / * ):")
 	fmt.Scanln(&operator)
 
 	switch operator {
 	case "+":
-		fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1+number2)
+		fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1+num2)
 	case "-":
-		fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1-number2)
+		fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1-num2)
 	case "*":
-		fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1*number2)
+		fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1*num2)
 
 	case "/":
-		if number2 == 0.0 {
+		if num2 == 0.0 {
 			fmt.Println("Divide by zero situation")
 		} else {
-			fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1/number2)
+			fmt.Printf("%f %s %f = %.3f", num1, number1, operator, number1/num2)
 
 		}
 	default:
